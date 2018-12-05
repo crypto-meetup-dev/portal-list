9b0b19928a0a6b197154418886ef90532bf4f0b6
lychees <lychees67@gmail.com>
2018-12-05 22:05:37 +0800

    Add sample portal.

---
+++ newportal.sh
@@ -0,0 +1,4 @@
+cleos wallet unlock --password PW5JaGpLtoM1vtD1WxiAC4RDsr82FRUczmKgocw1KJZqVahB4LZ1u
+
+
+cleos -u http://api.eosbeijing.one push action cryptomeetup newportal '[ "minakokojima", 0, 111, 20, 50, 350, 1000, 0]' -p cryptomeetup@owner eosotcbackup@active
